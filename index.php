916657871a8a4eb65ba644424c66edd675173ddc
Long Huỳnh Thanh <22520812@ms.uit.edu.vn>
2026-05-05 12:16:37 +0700

    Fix: Add Noto Sans Vietnamese font from Google Fonts to eliminate text encoding issues

--- index.php
+++ index.php
@@ -25,6 +25,7 @@ $csrf_token = $_SESSION['csrf_token'];
     <meta charset="UTF-8">
     <title>Shoe Store</title>
     <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;500;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
         integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
